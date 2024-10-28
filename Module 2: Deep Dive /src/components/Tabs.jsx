b6f10bca09d55d2217c666d651fc1f5b19e65718
Igor Cancar <icancar99@gmail.com>
2024-10-28 22:52:00 +0100

    Add default value for props

--- Module 2: Deep Dive /src/components/Tabs.jsx
+++ Module 2: Deep Dive /src/components/Tabs.jsx
@@ -1,5 +1,7 @@
-export default function Tabs({children, buttons, buttonsContainer}) {
+export default function Tabs({children, buttons, buttonsContainer = "menu"}) {
     const ButtonContainer = buttonsContainer
+    // if we left it with "buttonsContainer" when rendering it would look for custom component called buttonsContainer. 
+    // This identifier MUST start with uppercase letter if we want to use identifier for a custom component as a prop value.
     return (<>
         <ButtonContainer>
             {buttons}
