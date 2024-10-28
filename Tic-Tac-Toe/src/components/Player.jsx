138116c9a7f6d632ba6c62cec194f5b177463709
Igor Cancar <icancar99@gmail.com>
2024-10-28 23:56:57 +0100

    Implement two-way binding to get values from input from user

--- Tic-Tac-Toe/src/components/Player.jsx
+++ Tic-Tac-Toe/src/components/Player.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react"
-export default function Player({name, symbol}) {
+export default function Player({initialName, symbol}) {
     const [isEditing, setIsEditing] = useState(false);
     const [buttonText, setButtonText] = useState("Edit")
+    const [playerName, setPlayerName] = useState(initialName)
     function handleEditClick() {
         setIsEditing((editing) => !editing) //if we used setIsEditing(!isEditing) => this schedules state update to true which will not happen immediately. If we use this approach like is current, this function gets latest state of isEditing prop and it will happen immediately. You have guarrantee from react that you will have latest value in isEditing prop
         if(!isEditing) {
@@ -10,16 +11,20 @@ export default function Player({name, symbol}) {
             setButtonText("Edit")
         } 
     }
-    let playerName = <span className="player-name">{name}</span>
+
+    function handleNameChange(event) {
+        setPlayerName(event.target.value);
+    }
+    let editablePlayerName = <span className="player-name">{playerName}</span>
 
     if(isEditing) {
-        playerName = <input type="text" required value={name}></input>
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleNameChange}></input>
     }
 
     return (
         <li>
           <span className="player">
-            <span className="player-name">{playerName}</span>
+            <span className="player-name">{editablePlayerName}</span>
             <span className="player-symbol">{symbol}</span>
           </span>
           <button onClick={handleEditClick}>{buttonText}</button>
